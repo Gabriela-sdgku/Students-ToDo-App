d71183515b5ebc41e4bb4454b5a85cf8952c5c05
GabrielaAlcaraz <gabriela.alcaraz@cetys.mx>
2025-12-16 20:11:46 -0800

    data persistance

--- ToDo Task/ContentView.swift
+++ ToDo Task/ContentView.swift
@@ -2,16 +2,19 @@
 //  ContentView.swift
 //  ToDo Task
 //
-//  Created by Gabriela Sanchez on 09/12/25.
+//  Created SDGKU
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    @State private var taskGroups = TaskGroup.sampleData // See MockData
+    @State private var taskGroups: [TaskGroup] = []
     @State private var selectedGroup: TaskGroup? // selected group
     @State private var columnVisibility: NavigationSplitViewVisibility = .all // navigation side panel
     @State private var isShowingAddGroup = false
+    @Environment(\.scenePhase) private var scenePhase
+    @AppStorage("isDarkMode") private var isDarkMode = false
+    let saveKey = "savedTaskGroups"
     
     var body: some View {
         NavigationSplitView(columnVisibility: $columnVisibility) {
@@ -26,28 +29,68 @@ struct ContentView: View {
             .navigationTitle("ToDo APP")
             .listStyle(.sidebar)
             .toolbar {
-                Button {
-                    isShowingAddGroup = true
-                } label: {
-                    Image(systemName: "plus")
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        isDarkMode.toggle()
+                    } label: {
+                        Image(systemName: isDarkMode ? "sun.max.fill" : "moon.fill")
+                    }
+                }
+                
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        isShowingAddGroup = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
                 }
             }
-            
-        } detail : {
+        } detail: {
             if let group = selectedGroup {
-                if let index = taskGroups.firstIndex(where: {$0.id == group.id}) {
+                if let index = taskGroups.firstIndex(where: { $0.id == group.id }) {
                     TaskGroupDetailView(groups: $taskGroups[index])
                 }
             } else {
                 ContentUnavailableView("Select a Group", systemImage: "sidebar.left")
             }
         }
-        
         .sheet(isPresented: $isShowingAddGroup) {
             NewGroupView { newGroup in
                 taskGroups.append(newGroup)
-                selectedGroup = newGroup // automatically show up the details of the new group I created
             }
         }
+        .onAppear {
+            loadData()
+        }
+        .onChange(of: scenePhase) { oldValue, newValue in
+            if newValue == .active {
+                print("🟢 App is Active")
+            } else if newValue == .inactive {
+                print("🟡 App is Inactive")
+            } else if newValue == .background {
+                print("🔴 App is Background - Saving Data!")
+                saveData()
+            }
+        }
+        .preferredColorScheme(isDarkMode ? .dark : .light)
+
+    }
+    
+    func saveData() {
+        if let encodedData = try? JSONEncoder().encode(taskGroups){
+            UserDefaults.standard.set(encodedData, forKey: saveKey)
+        }
+    }
+    
+    func loadData() {
+        if let savedData = UserDefaults.standard.data(forKey: saveKey){
+            if let decodedGrpups = try? JSONDecoder().decode([TaskGroup].self, from: savedData) {
+                taskGroups = decodedGrpups
+                return
+            }
+        }
+        
+        // show mock data for dev purposes
+        taskGroups = TaskGroup.sampleData
     }
 }
