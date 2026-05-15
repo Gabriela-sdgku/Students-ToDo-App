b8bad607ec8bf1e897dda311f0bf1501a4de9eaf
GabrielaAlcaraz <gabriela.alcaraz@cetys.mx>
2026-05-14 17:13:15 -0700

    good

--- ToDo TaskTests/ToDo_TaskTests.swift
+++ ToDo TaskTests/ToDo_TaskTests.swift
@@ -7,6 +7,7 @@
 
 import Testing
 import Foundation
+import XCTest
 @testable import ToDo_Task
 
 struct Todo_TaskTests {
@@ -29,5 +30,26 @@ struct Todo_TaskTests {
         let pastDate = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
         let task = TaskItem(title: "Create Test Overdue", isCompleted: false, dueDate: pastDate)
         #expect(task.isOverdue == true, "A task with a past date and not completde should be overdue")
+    }
+}
+
+class ToDo_TaskTests: XCTestCase {
+    func testProgressCalculation() {
+        // GIVEN/ARRANGE: Have all the context (ingredients) ready to test
+        let tasks = [
+            TaskItem(title: "Test 1", isCompleted: true),
+            TaskItem(title: "Test 2", isCompleted: true),
+            TaskItem(title: "Text 3", isCompleted: false),
+            TaskItem(title: "Text 4", isCompleted: false)
+        ]
+        
+        let group = TaskGroup(title: "Group Test", symbolName: "star", tasks: tasks)
+            
+        //WHEN/ACT: Calculate the process
+        let completedCount = group.tasks.filter { $0.isCompleted}.count
+        let progress = Double(completedCount) / Double(group.tasks.count)
+        
+        // THEN/ASSERT: Compare the result to what I expect (2/4 = 0.5 = 50%)
+        XCTAssertEqual(progress, 0.5, "Progress should be 50% when half the tasks are done")
     }
 }
